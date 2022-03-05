9d64b9bbef762a51511819511166f742606d15e9
Kyle Kotowick <kotowick@invictonlabs.com>
2022-03-04 19:26:16 -0500

    Add `complete` output

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,14 @@ output "file_abspath" {
   depends_on = [
     data.external.create_file
   ]
-  description = "The absolute path to the file that was created."
+  description = "The absolute path to the file that was created. Does not return until the file has been created."
   value = var.file_abspath
+}
+
+output "complete" {
+  depends_on = [
+    data.external.create_file
+  ]
+  description = "Always `true`, but does not return until the file has been created."
+  value = true
 }
